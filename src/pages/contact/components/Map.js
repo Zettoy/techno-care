5dd7f82d8a124ffbfa8e66765138da601d1982f7
Zettoy <zed.yao@outlook.com>
2019-12-13 15:46:56 +1100

    fix

--- src/pages/contact/components/Map.js
+++ src/pages/contact/components/Map.js
@@ -2,5 +2,5 @@ import React from "react";
 import GoogleMap from "../../../util/GoogleMap";
 
 export default () => (
-  <div style={{height: '25rem'}}><GoogleMap/></div>
+  <div style={{display: 'flex', height: '25rem'}}><GoogleMap/></div>
 );
